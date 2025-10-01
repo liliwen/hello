8855446b06b5500ad55ce9a46ef1620ac8fbb9d8
liliwen <476059233@qq.com>
2025-10-01 19:21:48 +0800

    添加打印信息

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,6 @@ int main() {
   std::cout << "world" << std::endl;
   std::cout << "world2" << std::endl;
   std::cout << "world3" << std::endl;
+  std::cout << "world4" << std::endl;
   return 0;
 }
